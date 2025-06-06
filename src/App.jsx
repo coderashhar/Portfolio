1a476935f6b0a4fb25655360e23e10f05c9e694e
Mohd. Ashhar Khan <ashhar2003.khan@gmail.com>
2025-06-06 11:14:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import DeskBunny from "./DeskBunny";
+
+export default function App() {
+  return (
+    <div className="max-h-100"> 
+      <DeskBunny/>
+    </div>
+  );
+}
---
+++ src/DeskBunny.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function DeskBunny() {
+  return (
+    <Spline scene="https://prod.spline.design/ODKibCdwjc3bMn29/scene.splinecode" />
+  );
+}
